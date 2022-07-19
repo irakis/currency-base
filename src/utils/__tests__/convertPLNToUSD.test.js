7afc63659b61f26cd3bd7b1e4b7772487d84f413
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-07-19 17:58:13 +0200

    NaN not working

--- src/utils/__tests__/convertPLNToUSD.test.js
+++ src/utils/__tests__/convertPLNToUSD.test.js
@@ -1,4 +1,4 @@
-import { convertPLNToUSD } from './../convertPLNtoUSD';
+import { convertPLNToUSD } from '../convertPLNtoUSD';
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
@@ -6,5 +6,10 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(2)).toBe('$0.57');
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
-  });
+  })
+  it('should return NaN vlaue when input is text',()=>{
+    expect(convertPLNToUSD('7')).toBeNaN(2);
+    expect(convertPLNToUSD('abc')).toBeNaN(NaN);
+    expect(convertPLNToUSD('-11')).toBeNaN(NaN);
+  })
 });
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,19 @@
+
 export const convertPLNToUSD = (PLN) => {
+  console.log(PLN)
+  console.log('fire!')
+  console.log(isNaN(PLN));
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+if(isNaN(PLN)) {
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  console.log('use number')
+} else
+  {
+    const PLNtoUSD = PLN / 3.5;
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ')
+  } 
 }
